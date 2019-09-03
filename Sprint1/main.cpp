297654544186d999f114cd0b7708ad4c6601a86a
luisegarduno <luis.e.garduno@gmail.com>
2019-09-03 17:08:15 -0500

    command line

--- Sprint1/main.cpp
+++ Sprint1/main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main(int argc,char * argv[]){
+    if(argc > 0){
+        cout << "One Arg" << endl;
+    }
+
     return 0;
 }
+
+
+/*
+ * git config --global user.email "myEmail"
+ * git config --global user.name "myUsername"
+ * git add --all
+ * git status
+ * git commit -m "Comment"
+ * git push
+*/
