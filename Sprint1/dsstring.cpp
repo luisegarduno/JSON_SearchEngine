c17075c46f1b986e175000c8f1c6a2d7911de82a
luisegarduno <luis.e.garduno@gmail.com>
2019-09-04 20:29:41 -0500

    DSString class update

--- Sprint1/dsstring.cpp
+++ Sprint1/dsstring.cpp
@@ -11,8 +11,8 @@ DSString::DSString(const char* originalChar){                               // '
     strcpy(this->data,originalChar);                                        // c-copy 'originalChar' into 'data' array
 }
 
-DSString::DSString(const DSString& originaldsString){                   // Copy constructor, DSString parameter is a const,
-    this->data = new char[strlen(originaldsString.data) + 1];           // that goes by another name (&)
+DSString::DSString(const DSString& originaldsString){                       // Copy constructor, DSString parameter is a const,
+    this->data = new char[strlen(originaldsString.data) + 1];               // that goes by another name (&)
     strcpy(this->data,originaldsString.data);
 
 }
@@ -35,20 +35,20 @@ DSString& DSString::operator=(const DSString& originalString){              // r
     return *this;
 }
 
-DSString DSString::operator+(const DSString& originaldsString){
-    DSString newString;
-    strcpy(newString.data,originaldsString.data);
+DSString DSString::operator+(const DSString& originalString){             // returns by value
+    DSString newString = this->data;
+    strcat(newString.data,originalString.data);
 
     return newString;
 }
 
 DSString& DSString::operator+=(const DSString& originaldsString){
-    char * tempString = this->data;
-    this->data = new char[strlen(tempString) + strlen(originaldsString.data) + 1];
-    strcpy(this->data,tempString);
+    char * tempChar = this->data;
+    this->data = new char[strlen(tempChar) + strlen(originaldsString.data) + 1];
+    strcpy(this->data,tempChar);
     strcat(this->data,originaldsString.data);
 
-    delete [] tempString;
+    delete [] tempChar;
 
     return *this;                                                           // return a reference to invoking address
 }
@@ -71,13 +71,14 @@ bool DSString::operator<(const DSString&){
 
 char& DSString::operator[](const int){
 
+
 }
 
-int DSString::size(){
+int DSString::size(){                                                       // return by value
     return 0;
 }
 
-char* DSString::c_str(){
+char* DSString::c_str(){                                                    // returns by pointer
     return this->data;
 }
 
