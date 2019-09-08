2aacecb85ff24f2cf1533edce00aa5c0199200a0
luisegarduno <luis.e.garduno@gmail.com>
2019-09-07 23:05:00 -0500

    working on designing verbosity functions

--- Sprint1/lasertag.cpp
+++ Sprint1/lasertag.cpp
@@ -21,6 +21,23 @@ LaserTag::LaserTag(char * argv[]){
     //cout << "Team: " << TeamB.getTeamName() << endl;
     //cout << "Number of Team Members: " << TeamB.getNumOfMembers() << endl;
 
+
+    // cout << TeamA.getIDNum() << endl; //prints 15
+    // cout << TeamB.getIDNum() << endl; //prints 30
+    // Since TeamA < TeamB     TeamA: 1-15     TeamB: 16-30
+
+    DSString verbosity(argv[5]);
+    if(verbosity[1] == 'l'){
+        cout << "vlow" << endl;
+    }
+    else if(verbosity[1] == 'm'){
+        cout << "vmed" << endl;
+    }
+    else if(verbosity[1] == 'h'){
+        cout << "vhigh" << endl;
+    }
+
+
     DSString matchFileName(argv[3]);
     /*
      * Match(matchFileName){
