1dfbc1ffbd1003d23fef9aaed9078d323d3d9c3f
luisegarduno <luis.e.garduno@gmail.com>
2019-10-10 18:44:28 -0500

    Added Bubble, Insertion, & Selection Sort. Also added chrono library

--- Sprint3/Program3/dssorter.h
+++ Sprint3/Program3/dssorter.h
@@ -2,9 +2,35 @@
 #define DSSORTER_H
 
 #include <vector>
+#include <chrono>
 #include "sorter.h"
 
+class Timer{
+    private:
+        // Type aliases to make accessing nested type easier
+        using clock_t = std::chrono::high_resolution_clock;
+        using second_t = std::chrono::duration<double, std::ratio<1> >;
+
+        std::chrono::time_point<clock_t> m_beg;
+
+    public:
+        Timer() : m_beg(clock_t::now()){
+        }
+
+        void reset(){
+            m_beg = clock_t::now();
+        }
+
+        double elapsed() const{
+            return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+        }
+};
+
 class DSSorter{
+    private:
+        using clock_t = std::chrono::high_resolution_clock;
+        using second_t = std::chrono::duration<double, std::ratio<1>>;
+
     public:
         DSSorter(){}
         void test1(){
@@ -21,13 +47,22 @@ class DSSorter{
                 experiment[3] = new MysterySorterD<int>;
                 experiment[4] = new MysterySorterE<int>;
 
+                Timer t;
+                double n = 0;
+
+
                 for(int j = 0; j < 5; j++){
                     experiment[j]->setData(data);
+                    t.reset();
                     experiment[j]->sort();
+                    n = t.elapsed();
+                    std::cout << "Time taken: " << n << " seconds\n" << std::endl;
                 }
 
         }
 };
+
+
 
 
 #endif // DSSORTER_H
--- Sprint3/Program3/sorter.h
+++ Sprint3/Program3/sorter.h
@@ -23,39 +23,82 @@ class Sorter {
 template <typename T>
 class MysterySorterA : public Sorter<T> {
     public:
-        virtual void sort() {
+        virtual void sort() {   // Insertion sort
             std::cout << "Mystery Sorter A" << std::endl;
+            int i, j, tmp;
+            for(i = 1;i < this->data.size(); i++){
+                j = 1;
+                while(j > 0 && this->data[j - 1] > this->data[j]){
+                    tmp = this->data[j];
+                    this->data[j] = this->data[j - 1];
+                    this->data[j - 1] = tmp;
+                    j--;
+                }
+            }
         }
 };
 
 template <typename T>
 class MysterySorterB : public Sorter<T> {
     public:
-        virtual void sort() {
+        virtual void sort() {   // Selection Sort
             std::cout << "Mystery Sorter B" << std::endl;
+            int i, j, minIndex, tmp;
+            for (i = 0; i < this->data.size() - 1; i++) {
+                minIndex = i;
+                for (j = i + 1; j < this->data.size(); j++){
+                    if (this->data[j] < this->data[minIndex]){
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i) {
+                    tmp = this->data[i];
+                    this->data[i] = this->data[minIndex];
+                    this->data[minIndex] = tmp;
+                }
+            }
         }
 };
 
 template <typename T>
 class MysterySorterC : public Sorter<T> {
     public:
-        virtual void sort() {
+        virtual void sort() {   // Bubble Sort
             std::cout << "Mystery Sorter C" << std::endl;
+            bool swapped = true;
+            int j = 0;
+            int tmp;
+            while (swapped) {
+                swapped = false;
+                j++;
+                for (int i = 0; i < this->data.size() - j; i++) {
+                    if (this->data[i] > this->data[i + 1]) {
+                        tmp = this->data[i];
+                        this->data[i] = this->data[i + 1];
+                        this->data[i + 1] = tmp;
+                        swapped = true;
+                    }
+                }
+            }
         }
 };
 
 template <typename T>
 class MysterySorterD : public Sorter<T> {
+    private:
+
     public:
-        virtual void sort() {
+        virtual void sort() {   // Quick Sort
             std::cout << "Mystery Sorter D" << std::endl;
+
+
         }
 };
 
 template <typename T>
 class MysterySorterE : public Sorter<T> {
     public:
-        virtual void sort() {
+        virtual void sort() {   // Merge Sort
             std::cout << "Mystery Sorter E" << std::endl;
         }
 };
