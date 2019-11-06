1e2b1e4c34682590860a8ac60d312def5ddfe5a8
luisegarduno <luis.e.garduno@gmail.com>
2019-11-05 18:47:23 -0600

    Small adjustments being made in checkOuter and checkInner member class function's

--- Sprint4/Program4/adjacencylist.h
+++ Sprint4/Program4/adjacencylist.h
@@ -26,89 +26,65 @@ AdjacencyList<U>::AdjacencyList(){
 
 template<class U>
 void AdjacencyList<U>::add(U newFlightData){
-    U dataReverse = newFlightData.flightDataReversed();
-
+    U flightReverse = newFlightData.flightDataReversed();
     DSLinkedList<U> aList = checkOuter(newFlightData.getOrigin(), newFlightData.getAirline());             // Check list of orgination cities
 
-    if(adjacency_list.head != nullptr){                                  // if aList isn't empty
+    if(aList.head != nullptr){                                  // if adjacencyList.head isn't empty
+
         bool dataExists = checkInner(newFlightData.getDestination(),aList);    // Check if flight
 
         if(!dataExists){
-            //DSNode<U> flightNodeInformation;
             aList.append(newFlightData);                      // add destination to Origin Node (originCity)
 
-            aList = checkOuter(newFlightData.getDestination(),newFlightData.getAirline());
+
 
         }
     }
 
     else{
-
         DSLinkedList<U> newLinkedList;
         newLinkedList.append(newFlightData);
         adjacency_list.append(newLinkedList);                      // add this LinkedList to adjacencyList
-        bool dataExists = checkInner(newFlightData.getDestination(),newLinkedList);
-
-        if(!dataExists){
-            newLinkedList.append(newFlightData);
-        }
-
-        adjacency_list.append(newLinkedList);
     }
 }
 
 template<class U>
-DSLinkedList<U>& AdjacencyList<U>::checkOuter(DSString originData, DSString airlineData){          // check list of origination cities
+DSLinkedList<U>& AdjacencyList<U>::checkOuter(DSString originCity, DSString airlineData){          // check list of origination cities
     adjacency_list.resetIterator();
+    DSNode< DSLinkedList<U> >* currentLinkedList = adjacency_list.head;
 
-    static DSLinkedList<U> newCityLinkedList = DSLinkedList<U>();
 
 
-    if(adjacency_list.size == 0 && adjacency_list.iteratorIsValid() == false){
-        return adjacency_list.getIterator();
+    if(currentLinkedList->size == 0 && currentLinkedList->iteratorIsValid() == false){
+        return currentLinkedList->getIterator();
     }
 
     else {
-        while(adjacency_list.iteratorIsValid() != false || (adjacency_list.getIterator().head != originData)){
-            cout << adjacency_list.getIterator().head << endl;
-            adjacency_list.iterateForward();
+        while(currentLinkedList->iteratorIsValid() != false){
+            if(currentLinkedList->data.head->data.getOrigin() == originCity){
+                if(currentLinkedList->data.head->data.getAirline() == airlineData){
+                    break;
+                }
+            }
+            currentLinkedList->iterateForward();
         }
-
-        if(adjacency_list.iteratorIsValid() == false && adjacency_list.getIterator() != originData){
-            newCityLinkedList.append(originData);
-            return newCityLinkedList;
-        }
-
-        else {
-            newCityLinkedList = adjacency_list.getIterator();
-            return newCityLinkedList;
-        }
+        return this->currentLinkedList;
     }
 }
 
 template<class U>
-bool AdjacencyList<U>::checkInner(DSString city, const DSLinkedList<U>& flightList){
-    auto temp = flightList;
+bool AdjacencyList<U>::checkInner(DSString destinationCity, const DSLinkedList<U>& flightList){
 
-    temp.resetIterator();
+    DSLinkedList<U>* tempIterator = flightList.head;
 
     bool aFlag = false;
-    while(temp.iteratorIsValid()){
-        if(temp.getIterator() == city){
+    while(tempIterator->iteratorIsValid() != false){
+        if(tempIterator->head->data == destinationCity){
             aFlag = true;
+            break;
         }
-        else{
-            temp.iterateForward();
-        }
+            tempIterator->iterateForward();
     }
-
-    auto temp2 = flightList.iterator;
-    auto temp3 = temp2.getOrigin();
-
-
-
-
-
     return (aFlag) ? true : false;
 }
 
