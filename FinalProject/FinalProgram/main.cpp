caf0c07916fb6a440ed0fbb9cb882f6b8465212b
luisegarduno <Luis.e.garduno@gmail.com>
2019-11-13 17:18:09 -0600

    Test

--- FinalProject/FinalProgram/main.cpp
+++ FinalProject/FinalProgram/main.cpp
@@ -1,10 +1,14 @@
 #include "mode.h"
 
 #include <QApplication>
+#include <iostream>
+using std::cout;
+using std::endl;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    cout << "howdy" << endl;
     Mode w;
     w.show();
     return a.exec();
